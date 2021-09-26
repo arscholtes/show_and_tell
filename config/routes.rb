7c16ce73c443a952980d75d21f6288dcb47bf267
Th3MFG <arscholtes@gmail.com>
2021-09-26 16:15:18 -0400

    Conversation feature looking good so far

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
+# Language: Ruby, Level: Level 4
 Rails.application.routes.draw do
+  root 'conversations#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
