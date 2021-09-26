f72b41f728fb4baf3f42d011ed1fd88b1e9f6eae
Th3MFG <arscholtes@gmail.com>
2021-09-26 04:24:43 -0400

    Spider is supposedly done, probably breaking somewhere but we will test it later, also have a demo scraper working in a different file.  Remember to push that up later once I come to test this

--- webcrawler.rb
+++ webcrawler.rb
@@ -1,10 +1,28 @@
+# Language: Ruby, Level: Level 3
+# A web crawler in Ruby
+#
+# This script provides a generic Spider class for crawling urls and
+# recording data scraped from websites. The Spider is to be used in
+# collaboration with a "processor" class that defines which pages to
+# visit and how data from those pages should be consumed.
+#
+# Usage:
+#   spider = ProgrammableWeb.new
+#   spider.results.take(10)
+#   => [{...}, {...}, ...]
+#
+# Requirements:
+#   Ruby 2.0+
+#
+# Based on Rossta's blog post about webcrawlers
+#
 require "mechanize"
+require "pry"
 
 class Spider
 
   def initialize(processor, attrs = {})
     @processor = prcessor
-    # Zero everything out.
     @urls      = []
     @results   = []
     @handlers  = []
@@ -33,8 +51,8 @@ class Spider
   def results
     return enum_for(:results) unless block_given?
 
-    i = @results.length
-    enqqueued_urls.each do |url, handler|
+    index = @results.length
+    enqueued_urls.each do |url, handler|
 
       # Process the urls
       @processor.send(handler[:method], agent.get(url), handler[:data])
@@ -50,22 +68,22 @@ class Spider
   end
 
   private
-
+# Itterate through URL queue
   def enqueued_urls
     Enumerator.new do |y|
       index = 0
       while index < @urls.count && index <= @max_urls
         url = @urls[index]
-        index +=
+        index += 1
         next unless url
         y.yield url, @handlers[url]
       end
     end
   end
 
-
-
-  private
+  def log(label, info) # Not part of the article
+    warn "%-10s: %s" % [label, info]
+  end
 
   def agent
     @agent ||= Mechanize.new
@@ -73,15 +91,70 @@ class Spider
 end
 
 
-# No clue where this is going to go quite yet or how to
-# require "pstore"
-# store  = PStore.new("api_directore.pstore")
+# Wrap spider and extract data
 
-# create `spider`, then ...
 
-# spider.results.lazy.take(50).each_with_index do |result, i|
-#   store.transaction do
-#     store[result[:name]] = result
-#     store.commit
-#   end
-# end
+class Techpoint
+  attr_reader :root, :handler
+
+  def initialize(root: "https://techpoint.org/", handler: :process_index, **options)
+    @root = root
+    @handler = handler
+    @options = options
+  end
+
+# Grab api names from index list
+
+def process_index(page, data = {})
+  page.links_with(href: %r{\?page=\d+}).each do |link|
+    spider.enqueue(link.href, :process_index)
+  end
+
+  page.links_with(href: %r{/api/\w+$}).each do |link|
+    spider.enqueue(link.href, :process_api, name: link.text)
+  end
+end
+
+# Grab
+
+# def process_api(page, data = {})
+#   fields = page.search("#{tabs-content .field}").each_with_object({}) do |tag, o|
+#    key = tag.search("label").text.strip.downcase.gsub(%r{[^\w]+}, ' ').gsub(%r{\s+}.to_sym)
+#    val = tag.search("span").text
+#    o[key] = val
+#  end
+#
+#  categories = page.search("article.node-api .tags").first.text.strip.split(/\s+/)
+#
+#  spider.record data.merge(fields).merge(categories: categories)
+#end
+
+  def process_api(page, data = {})
+  categories = page.search("article.node-api .tags").first.text.strip.split(/\s+/)
+  fields = page.search("#tabs-content .field").each_with_object({}) do |tag, results|
+    key = tag.search("label").text.strip.downcase.gsub(/[^\w]+/, ' ').gsub(/\s+/, "_").to_sym
+    val = tag.search("span").text
+    results[key] = val
+  end
+
+  spider.record data.merge(fields).merge(categories: categories)
+end
+
+def results(&block)
+  spider.results(&block)
+end
+
+  private
+
+  def spider
+    @spider ||= Spider.new(self, @options)
+  end
+end
+
+if __FILE__ == $0
+  spider = Techpoint.new
+
+  spider.results.lazy.take(5).each_with_index do |result, i|
+    warn "%-2: %s" % [i, result.inspect]
+  end
+end
